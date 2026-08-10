7cd9250db934742557ec16708de0d2d5861eef79
Daniel-Antonio-Bandeira <dantonioband@gmail.com>
2026-08-09 21:28:18 -0300

    adicionando index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express();
+const port = 3000;
+
+// Endpoint para converter decimal para binario
+app.get("/to-binary/:decimal", (req, res) =>{
+    // Converte o parametro para numero inteiro
+    const   decimal = parseInt(req.params.decimal, 10)
+
+    // se nao for numero decimal retorna um erro com status 400
+    if(isNaN(decimal)){
+        return res.status(400).json({ erro: "Invalid decimal number"})
+        }
+
+    const binary = decimal.toString(2); //converte o numero decimal para binario
+    res.json({ decimal, binary }); // retorna a resposta como JSON
+    });
+
+    app.listen(port, () => {
+        console.log(`Servidor rodando na porta ${port}`)
+    })
