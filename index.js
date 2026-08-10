37b6a3d23d386947c858a459250146390d0322b3
Daniel-Antonio-Bandeira <dantonioband@gmail.com>
2026-08-09 21:52:09 -0300

    Add conversion from decimal to hexadecima

--- index.js
+++ index.js
@@ -20,3 +20,18 @@ app.get("/to-binary/:decimal", (req, res) =>{
     app.listen(port, () => {
         console.log(`Servidor rodando na porta ${port}`)
     })
+
+    //Novo endpoint para converter decimal para hexadecimal
+    app.get("/to-hex/:decimal", (req, res) => {
+        const decimal = parent(req.params.decimal, 10)
+        if(isNaN(decimal)) {
+            return req.status(400).json({ erro: "Invadi decimal number"})
+        }
+        //Converte para hexadecimal e deixa em maiusculas
+        const hex = decimal.toString(16).toUpperCase();
+        res.json({decimal, hex})
+    })
+
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`)
+    })
